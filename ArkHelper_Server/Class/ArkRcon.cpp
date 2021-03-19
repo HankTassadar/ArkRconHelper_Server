be6ca3b4f721ba8d4676ec2beb09cc0976888669
HankTassadar <1101638527ljqsn@gmail.com>
2021-03-19 17:35:23 +0800

    update lib for VS2019

--- ArkHelper_Server/Class/ArkRcon.cpp
+++ ArkHelper_Server/Class/ArkRcon.cpp
@@ -26,21 +26,21 @@ bool ArkRcon::init()
 
 void ArkRcon::update()
 {
-	for (auto &i : this->_server) {
+	for (auto& i : this->_server) {
 		i->clearRecv();
 		i->updatePlayerList();
 	}
 }
 
-void ArkRcon::broadcast(std::string &data)
+void ArkRcon::broadcast(std::string& data)
 {
-	for (auto &i : this->_server)
+	for (auto& i : this->_server)
 		i->broadcast(data);
 }
 
 void ArkRcon::updateGameName()
 {
-	for (auto &i : this->_server)
+	for (auto& i : this->_server)
 		i->updateGameName();
 }
 
@@ -48,11 +48,12 @@ bool ArkRcon::addServer(Rcon_addr addr)
 {
 	auto server = new ArkServer();
 	auto flag = server->init(addr);
-	if (flag) { 
-		LOG(server->getServerName()+"连接成功");
+	if (flag) {
+		LOG(server->getServerName() + "连接成功");
 		server->updatePlayerList();
 		this->_server.push_back(server);
-		return true; }
+		return true;
+	}
 	LOG(server->getServerName() + "连接失败");
 	delete(server);
 	return false;
